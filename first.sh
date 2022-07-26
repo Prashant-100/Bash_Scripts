c7c5d000d695342fd02276ed388c1c6afbd56ec9
Prashant Dwivedi <77194839+Prashant-100@users.noreply.github.com>
2022-07-26 19:58:30 +0530

    multi situation sh comment

---
+++ first.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -x
+
+#var=20
+
+#if [ "$var" -lt 15 ]
+#then :
+#else
+#       echo $var
+#fi
+
+#let val=500/2
+
+#val2=`echo $val`
+
+#echo "$val2"
+
+
+
+
+
+#let val=500/2
+#echo $val
+
+
+#echo "\"Linux is Awesome\""
+
+
+
+#var=DSLConNecTiON
+
+#echo ${var,,}
+
+
+
+
+#let "y=((x=20, 10/2))"
+
+#echo $y
+
+
+
+#colors="red black white"
+#echo $colors
+
+#for col in "$colors"
+#do
+#       echo $col
+#done
+
+#colors="red black white"
+#echo $colors
+
+#for col in $colors
+#do
+#       echo $col
+#done
+
+
+
+#name=prashant
+#echo "The word $name contains ${#name} chars."
+#echo $((2#111))
+
+#var=10
+
+#if [ "$var" -gt 0 ]; then echo "YES"; else echo "NO"
+#fi
